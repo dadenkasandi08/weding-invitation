41a474a400a1c18e6542fce9f1adfb3790500a79
dadenkasandi08 <dadenkasandi43@mhs.unram.ac.id>
2024-08-14 21:39:24 +0800

    tambah lagu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import InvitationSectionOne from "./components/InvitationSectionOne";
 import InvitationSectionTwo from "./components/InvitationSectionTwo";
 import InvitationSectionThree from "./components/InvitationSectionThree";
+import audio from "./bg-music.mp3";
 
 export default function App(){
+
+    const audioRef = useRef(null);
+
+    useEffect(() =>{
+        audioRef.current.play().catch(
+            (error) => {console.error("Error playing audio", error);}
+        );
+    },[]);
 
     return(
         <div className="invitation-container">
             <InvitationSectionOne />
             <InvitationSectionTwo />
             <InvitationSectionThree />
+            <audio ref={audioRef} src={audio} autoPlay />
         </div>
     );
 }
